3436ca44f3b9a8d516a9e2ed8146abc875564f18
Maxim <maxidebox@list.ru>
2022-09-24 14:08:00 +0400

    Update Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyD9UX7ljOK59JFmBv2BwVjiv_Twrg0q7iU",
+    authDomain: "social-post-app-4e55b.firebaseapp.com",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "social-post-app-4e55b",
+    storageBucket: "social-post-app-4e55b.appspot.com",
+    messagingSenderId: "536460780913",
+    appId: "1:536460780913:web:fbf681203c77af336342d0",
+    measurementId: "G-VH35Z3T8CL"
+  }
 };
